6bd1eb20d9f7c4b706e18747562078350f3dc1d4
unknown <sebastienguidez59@gmail.com>
2020-05-05 14:37:48 +0200

    probleme h 1999 ok + avancement 2000 B

--- 1999/problem_H/main.c
+++ 1999/problem_H/main.c
@@ -7,23 +7,23 @@
 int main() {
     int cas = 0;
     int n, m, A[1058];
-    double V, h= 0, sum = 0;
+    double volCub,lastLine, waterLev= 0, sum = 0;
 
     while (scanf("%d %d", &n ,&m) == 2) {
          if (n < 30 && m < 30){
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 scanf("%d", &A[i*m+j]);
-        scanf("%lf", &V);
-        printf("0 0\n");
+        scanf("%lf", &volCub);
+        scanf("%lf", &lastLine);
         n = n*m;
         A[n] = 1*exp(10);
         printf("\nRegion %d\n", ++cas);
         for (int i = 0; i < n; i++) {
             sum += A[i];
-            h = (V+sum*100)/(i+1)/100.0;
-            if (h < A[i+1]+eps) {
-                printf("Water level is %.2lf meters.\n", h);
+            waterLev = (volCub+sum*100)/(i+1)/100.0;
+            if (waterLev < A[i+1]+eps) {
+                printf("Water level is %.2lf meters.\n", waterLev);
                 printf("%.2lf percent of the region is under water.\n\n", (i+1)*100.0/n);
                 break;
             }
--- 2000/problem_B/main.c
+++ 2000/problem_B/main.c
@@ -1,43 +1,94 @@
 #include <stdio.h>
 #include <string.h>
 
+int sol;
+char path[1024];
 
-void calculSol(char expr[]) {
-//faire l operation
+void checkSum(int *place, int sum, char expr[], int index)
+{
+	for (int i = 0; i < index; i++)
+		sum += place[i];
+	if (sum == 2000)
+	{
+		printf("  %s=\n", expr);
+		sol++;
+	}
 }
 
-void defIndex(int index, int place, int n, char *s) {
-    char path[1024];
+void calculSol(char expr[])
+{
+	int n = 0;
+	int place[20];
+	int index = 0;
+	char oper = '+';
+	int sum = 0;
+	n = strlen(expr);
+	for (int i = 0; i < n;)
+	{
+		if (expr[i] >= '0' && expr[i] <= '9')
+		{
+			int num = 0;
+			if (expr[i] == '0' && expr[i + 1] >= '0' && expr[i + 1] <= '9')
+				return; // 01, 001, ...
+			while (expr[i] >= '0' && expr[i] <= '9')
+			{
+				num = num * 10 + expr[i] - '0';
+				i++;
+			}
+			if (oper == '*')
+				place[index - 1] *= num;
+			else if (oper == '-')
+				place[index++] = -num;
+			else if (oper == '+')
+				place[index++] = num;
+		}
+		else
+		{
+			oper = expr[i];
+			i++;
+		}
+	}
+	checkSum(place, sum, expr, index);
+}
+
+void defIndex(int index, int place, int n, char *s)
+{
 	char operator[] = "*+-";
-	if (index == n) {
+	if (index == n)
+	{
 		path[place] = '\0';
 		if (place > n)
-		//calculSol(path);
-		return ;
+			calculSol(path);
+		return;
 	}
-	if (index != n-1) {
-	for (int i = 0; i < 3; i++) {
+	if (index != n - 1)
+	{
+		for (int i = 0; i < 3; i++)
+		{
 			path[place] = s[index], path[place + 1] = operator[i];
-			defIndex(index+1, place+2, n,s);
+			defIndex(index + 1, place + 2, n, s);
 		}
 	}
 	path[place] = s[index];
-	defIndex(index+1, place +1, n,s);
+	defIndex(index + 1, place + 1, n, s);
 }
 
-int main() {
-	int sol,n;
+int main()
+{
+	int n;
 	int cases = 0;
-char s[1024];
-	while (scanf("%s", s) == 1) {
+	char s[1024];
+	printf("Veuillez rentrer une operation ou rentrer = pour quitter\n");
+	while (scanf("%s", s) == 1)
+	{
 		if (!strcmp(s, "="))
 			break;
+        sol = 0;
 		n = strlen(s);
 		s[n - 1] = '\0';
-		sol = 0;
-		printf("Problem %d\n", ++cases);
 		n--;
-		defIndex(0, 0, n,s);
+		printf("Probleme %d\n", ++cases);
+		defIndex(0, 0, n, s);
 		if (sol == 0)
 			printf("IMPOSSIBLE\n");
 	}
